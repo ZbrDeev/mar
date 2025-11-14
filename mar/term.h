6815840415d1a674d342714de337698c3b29a01b
ZbrDeev <github.cannot978@passinbox.com>
2025-11-14 23:39:30 +0100

    Add should_close variable and fix some problem in init_term with the oflag

--- mar/term.h
+++ mar/term.h
@@ -3,6 +3,7 @@
 
 #include "utf8.h"
 #include <termios.h>
+#include <stdbool.h>
 
 #define MAX_LINE 100
 #define MAX_COL 100
@@ -11,6 +12,7 @@ static struct termios newterm, oldterm;
 
 static int line = 1;
 static int column = 1;
+static bool should_close = false;
 
 void init_term(void);
 
