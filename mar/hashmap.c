4318d769b88ee3575b48903e74979d4fad2bdbb3
ZbrDeev <github.cannot978@passinbox.com>
2025-11-16 23:40:12 +0100

    Fix some problem with hashmap and change function name

--- mar/hashmap.c
+++ mar/hashmap.c
@@ -1,10 +1,10 @@
 #include "hashmap.h"
 #include <stdlib.h>
 
-struct hashmap init_hashmap(void){
+struct hashmap h_init(void){
     struct hashmap hash;
 
-    hash.nodes = (struct node**)malloc(sizeof(struct node*));
+    hash.nodes = (struct node**)malloc(sizeof(struct node*) * MAX_CAPACITY);
 
     for(size_t i = 0; i < MAX_CAPACITY; ++i){
         hash.nodes[i] = NULL;
@@ -13,7 +13,7 @@ struct hashmap init_hashmap(void){
     return hash;
 }
 
-struct node* navigate_in_node_hashmap(struct node* np, unsigned key){
+struct node* h_navigate_in_node(struct node* np, unsigned key){
     while(np != NULL && np->next != NULL){
         if(np->key == key){
             return np;
@@ -25,9 +25,13 @@ struct node* navigate_in_node_hashmap(struct node* np, unsigned key){
     return NULL;
 }
 
-struct node* get_value_hashmap(struct hashmap* hp, unsigned key){
+struct node* h_get_value(struct hashmap* hp, unsigned key){
     unsigned key_hashed = CALC_KEY_INDEX(key);
     struct node* np = hp->nodes[key_hashed];
+
+    if(np == NULL){
+        return NULL;
+    }
 
     while(np->next != NULL){
         if(np->key == key){
@@ -37,31 +41,43 @@ struct node* get_value_hashmap(struct hashmap* hp, unsigned key){
         np = np->next;
     }
 
+    if(np->key == key){
+        return np;
+    }
+
     return NULL;
 }
 
-void insert_value_hashmap(struct hashmap* hp, unsigned key, hashmap_function* function){
+void h_insert_value(struct hashmap* hp, unsigned key, hashmap_function_t* function){
     unsigned key_hashed = CALC_KEY_INDEX(key);
-    struct node* np = hp->nodes[key_hashed];
 
-    while(np->next != NULL || np->key == key){
-        np = np->next;
-    }
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->key = key;
+    new_node->function = function;
+    new_node->next = NULL;
 
     
-    if(np == NULL){
-        np = (struct node*)malloc(sizeof(struct node));
-        np->next = NULL;
+    if(hp->nodes[key_hashed] == NULL){
+        hp->nodes[key_hashed] = new_node;
+    }else{
+        new_node->next = hp->nodes[key_hashed];
+        hp->nodes[key_hashed] = new_node;
     }
-
-    np->key = key;
-    np->function = function;
 }
 
-void free_hashmap(struct hashmap* hp){
+static void free_node(struct node* np){
+    if(np == NULL){
+        return;
+    }
+
+    free_node(np->next);
+    free(np);
+}
+
+void h_free(struct hashmap* hp){
     for(size_t i = 0; i < MAX_CAPACITY; ++i){
         if(hp->nodes[i] != NULL){
-            free(hp->nodes[i]);
+            free_node(hp->nodes[i]);
         }
     }
 
--- mar/hashmap.h
+++ mar/hashmap.h
@@ -4,11 +4,11 @@
 #define MAX_CAPACITY 10
 #define CALC_KEY_INDEX(key) (key % MAX_CAPACITY)
 
-typedef void (hashmap_function)(void);
+typedef void (hashmap_function_t)(void);
 
 struct node {
     unsigned key;
-    hashmap_function* function;
+    hashmap_function_t* function;
 
     struct node* next;
 };
@@ -17,14 +17,14 @@ struct hashmap {
     struct node** nodes;
 };
 
-struct hashmap init_hashmap(void);
+struct hashmap h_init(void);
 
-struct node* navigate_in_node_hashmap(struct node* np, unsigned key);
+struct node* h_navigate_in_node(struct node* np, unsigned key);
 
-struct node* get_value_hashmap(struct hashmap* hp, unsigned key);
+struct node* h_get_value(struct hashmap* hp, unsigned key);
 
-void insert_value_hashmap(struct hashmap* hp, unsigned key, hashmap_function* function);
+void h_insert_value(struct hashmap* hp, unsigned key, hashmap_function_t* function);
 
-void free_hashmap(struct hashmap* hp);
+void h_free(struct hashmap* hp);
 
 #endif // HASHMAP_H
