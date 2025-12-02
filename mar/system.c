4ebb12e4b9be4895655bd5c532f0f9a180363b06
ZbrDeev <github.cannot978@passinbox.com>
2025-12-02 15:07:33 +0100

    Add system file and add copy functionnality

---
+++ mar/system.c
@@ -0,0 +1,20 @@
+#include "system.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void copy_to_clipboard(char* value, size_t size){
+    char* session_type = getenv("XDG_SESSION_TYPE");
+    FILE* fp = NULL;
+
+    if(strcmp(session_type, "wayland") == 0){
+        fp = popen("wl-copy", "w");
+    }else if(strcmp(session_type, "x11") == 0){
+        fp = popen("xclip -selection clipboard", "w");
+    }else{
+        return;
+    }
+
+    fwrite(value, 1, size, fp);
+    pclose(fp);
+}
---
+++ mar/system.h
@@ -0,0 +1,8 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <stddef.h>
+
+void copy_to_clipboard(char* value, size_t size);
+
+#endif // SYSTEM_H
